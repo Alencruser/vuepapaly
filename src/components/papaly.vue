89e20c129aa085dc983b478c6df04e11521d3d62
Valentin <Alencruserxx@hotmail.fr>
2018-10-25 15:58:47 +0200

    stockage de toutes les categories ainsi que leurs liens fonctionnels

--- src/components/papaly.vue
+++ src/components/papaly.vue
@@ -4,15 +4,16 @@
 			<h3>Ajouter un lien</h3>
 			<input type="text" v-model="link">
 			<input type="text" v-model="linkname">
-			<select name="">
-				<option v-for="link in categorylist" :value="link">{{link}}</option>
+			<select name="" v-model="categorychoose">
+				<option disabled value="">Status</option>
+				<option v-for="link in category" :value="link">{{link}}</option>
 			</select>
-			<button>Ajouter un lien</button><br>
+			<button @click="add">Ajouter un lien</button><br>
 			<h3>Ajouter une catégorie</h3>
 			<input type="text" v-model="categoryAdd">
 			<button @click="create">Ajouter votre catégorie</button>
 		</div>
-		<div v-for="cat in categorylist" :id="cat"></div>
+		<div v-for="cat in category" :id="cat"><p v-for="link in prepareDom.cat">{{link}}</p></div>
 	</div>
 </template>
 <script>
@@ -24,18 +25,25 @@
 				categoryAdd:'',
 				link:'',
 				linkname:'',
-				categorylist:[]
+				categorylist:[],
+				prepareDom:{},
+				categorychoose:''
 			}
 		},
 		methods: {
 			create: function(){
 				if(!this.category.includes(this.categoryAdd)){
-					console.log('ajout de la category')
 					this.category.push(this.categoryAdd)
-					this.categorylist.push(this.categoryAdd)
 				}else{
-					alert('cette catégorie existe déjà robert')
+					alert('Cette catégorie existe déjà robert')
 				}
+			},
+			add: function(){
+				if(!this.prepareDom[this.categorychoose]){
+				this.prepareDom[this.categorychoose]=[this.link]
+			}else{
+				this.prepareDom[this.categorychoose].push(this.link)
+			}
 			}
 		}
 	}
