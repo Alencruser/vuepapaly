adcae1da174d9acda2e02ae0ffd5a04b501d3b11
Valentin <Alencruserxx@hotmail.fr>
2018-10-25 14:18:06 +0200

    ajout des categories si elles n'existent pas déjà ainsi que ajout de l'option si l'ajout est un succès

---
+++ src/components/papaly.vue
@@ -0,0 +1,41 @@
+<template>
+	<div id="papa">
+		<div id="inputs">
+			<h3>Ajouter un lien</h3>
+			<input type="text" v-model="link">
+			<input type="text" v-model="linkname">
+			<select name="">
+				<option v-for="link in categorylist" :value="link">{{link}}</option>
+			</select>
+			<button>Ajouter un lien</button><br>
+			<h3>Ajouter une catégorie</h3>
+			<input type="text" v-model="categoryAdd">
+			<button @click="create">Ajouter votre catégorie</button>
+		</div>
+	</div>
+</template>
+<script>
+	export default {
+		name:"papaly",
+		data:function(){
+			return {
+				category:[],
+				categoryAdd:'',
+				link:'',
+				linkname:'',
+				categorylist:[]
+			}
+		},
+		methods: {
+			create: function(){
+				if(!this.category.includes(this.categoryAdd)){
+					console.log('ajout de la category')
+					this.category.push(this.categoryAdd)
+					this.categorylist.push(this.categoryAdd)
+				}else{
+					alert('cette catégorie existe déjà robert')
+				}
+			}
+		}
+	}
+</script>
