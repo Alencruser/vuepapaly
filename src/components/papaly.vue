213b59ef011ecdf9041f9e9ac43863ed4db0fac8
Valentin <Alencruserxx@hotmail.fr>
2018-10-26 14:31:58 +0200

    sécurité si aucune categorie n'est choisi

--- src/components/papaly.vue
+++ src/components/papaly.vue
@@ -2,15 +2,15 @@
 	<div id="papa">
 		<div id="inputs">
 			<h3>Ajouter un lien</h3>
-			<input type="text" v-model="link">
-			<input type="text" v-model="linkname">
+			<input placeholder="Lien du site" type="text" v-model="link">
+			<input placeholder="Nom de votre lien" type="text" v-model="linkname">
 			<select name="" v-model="categorychoose">
 				<option disabled value="">Aucune</option>
 				<option v-for="link in category" :value="link">{{link}}</option>
 			</select>
 			<button @click="add">Ajouter un lien</button><br>
 			<h3>Ajouter une catégorie</h3>
-			<input type="text" v-model="categoryAdd">
+			<input placeholder="Nom de votre catégorie" type="text" v-model="categoryAdd">
 			<button @click="create">Ajouter votre catégorie</button>
 		</div>
 		<div v-for="cat in category" :id="cat"><h2>{{cat}}</h2><a v-for="link in prepareDom[cat]" :href="link[0]" target="_blank">{{link[1]}}<br></a></div>
@@ -40,6 +40,9 @@
 				}
 			},
 			add: function(){
+				if(this.categorychoose==''){
+					alert('Vous n\'avez pas choisi de catégories, veuillez en choisir une ou la créer')
+				}
 				if(!this.prepareDom[this.categorychoose]){
 				this.prepareDom[this.categorychoose]=[[this.link,this.linkname]]
 				this.link=''
